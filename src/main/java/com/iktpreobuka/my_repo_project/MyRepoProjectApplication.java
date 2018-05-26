e2791b03deef9d3d97f304ccb50eb21e051988ea
zevs007 <shajin.davor@gmail.com>
2018-05-26 12:05:17 +0200

    commit message

---
+++ src/main/java/com/iktpreobuka/my_repo_project/MyRepoProjectApplication.java
@@ -0,0 +1,12 @@
+package com.iktpreobuka.my_repo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRepoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRepoProjectApplication.class, args);
+	}
+}
